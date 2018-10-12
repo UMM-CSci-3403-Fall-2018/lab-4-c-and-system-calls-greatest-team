49e63b4b40899ddd392b008d38884fa9452bdbef
Mark Schauer <marksthe1@gmail.com>
2018-10-12 13:10:30 -0500

    Write code to `summarize_tree_ftw.c`, but tests are not passing

--- summarize_tree/summarize_tree_ftw.c
+++ summarize_tree/summarize_tree_ftw.c
@@ -4,19 +4,24 @@
 #include <ftw.h>
 #include <sys/stat.h>
 
-static int callback(const char *fpath, const struct stat *sb, int typeflag)
-// and pass it in as the second argument of ftw(), something like this:
+#define MAX_FTW_DEPTH 16
+static int num_dirs, num_reg;
 
 static int callback(const char *fpath, const struct stat *sb, int typeflag) {
-    // Define stuff here
+    if (typeflag == FTW_F) {
+      ++num_reg;
+    } else if (typeflag == FTW_D){
+      if(strcmp(fpath, ".") != 0 || strcmp(fpath, "..") != 0) {
+        ++num_dirs;
+      }
+    }
+    return 0;
 }
 
-#define MAX_FTW_DEPTH 16
 
 int main(int argc, char** argv) {
-    // Check arguments and set things up
-
+    num_dirs = 0, num_reg = 0;
     ftw(argv[1], callback, MAX_FTW_DEPTH);
-
-    // Print out the results
+    printf("There are %d directories. \n", num_dirs);
+    printf("There are %d regular files. \n", num_reg);
 }
